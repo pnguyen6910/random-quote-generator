609343397208539fc3d5c05301ab2b6360273c31
Paul Nguyen <pnguyen6910@gmail.com>
2022-03-30 18:39:38 -0500

    setting up framework and files

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import axios from 'axios'
+import './App.css'
+
+class App extends React.Component {
+    state = { advice: '' }
+
+    componentDidMount() {
+        console.log('component did mount')
+    }
+
+    render() {
+        return (
+            <h1>APP</h1>
+        )
+    }
+}
+
+export default App
