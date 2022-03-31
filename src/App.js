124229dbbcbd28e416578d4a45599f9093e7fef8
Paul Nguyen <pnguyen6910@gmail.com>
2022-03-30 20:03:01 -0500

    added more styling to button

--- src/App.js
+++ src/App.js
@@ -1,38 +1,39 @@
-import React from 'react'
-import axios from 'axios'
-import './App.css'
+import React from "react";
+import axios from "axios";
+import "./App.css";
 
 class App extends React.Component {
-    state = { advice: '' }
+  state = { advice: "" };
 
-    componentDidMount() {
-        this.fetchAdvice()
-    }
+  componentDidMount() {
+    this.fetchAdvice();
+  }
 
-    fetchAdvice = () => {
-        axios.get('https://api.adviceslip.com/advice')
-        .then((response) => {
-            this.setState({ advice: response.data.slip.advice})
-        })
-        .catch((error) => {
-            console.log(error)
-        })
-    }
+  fetchAdvice = () => {
+    axios
+      .get("https://api.adviceslip.com/advice")
+      .then((response) => {
+        this.setState({ advice: response.data.slip.advice });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-    render() {
-        const { advice } = this.state
+  render() {
+    const { advice } = this.state;
 
-        return (
-            <div className='app'>
-                <div className='card'>
-                    <h1 className='heading'>{ advice }</h1>
-                    <button className='button' onClick={this.fetchAdvice}>
-                        <span>GIVE ME ADVICE</span>
-                    </button>
-                </div>
-            </div>
-        )
-    }
+    return (
+      <div className="app">
+        <div className="card">
+          <h1 className="heading">{advice}</h1>
+          <button className="button" onClick={this.fetchAdvice}>
+            <span>GIVE ME ADVICE</span>
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
